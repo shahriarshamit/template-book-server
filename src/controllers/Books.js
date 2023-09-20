4c3a1915b9827677b510c9f122448d60a43e0371
Khandaker Shahriar Shamit <samdevstudio@gmail.com>
2023-09-21 01:13:45 +0600

    Added Category Filter

--- src/controllers/Books.js
+++ src/controllers/Books.js
@@ -7,7 +7,12 @@ connect();
 
 const getAllBooks = async function (request, response) {
     try {
-        const data = await BookModel.find({});
+        const category = request.query.category;
+        const filter = {};
+        if (category) {
+            filter.category = category;
+        }
+        const data = await BookModel.find(filter);
         return response.status(200).json({status: 'success', data: data});
     } catch (error) {
         return response.status(500).json({status: 'error', data: 'An error occured while fetching books list'});
--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
 
 app.get("/", function (req, res) {
     res.json('Hello World');
