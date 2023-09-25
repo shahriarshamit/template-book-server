f15e41d75cc55f2f75636114ea2b30972ee69a35
Khandaker Shahriar Shamit <samdevstudio@gmail.com>
2023-09-25 13:00:26 +0600

    Book Create Route Added

--- src/controllers/Books.js
+++ src/controllers/Books.js
@@ -22,7 +22,6 @@ const getAllBooks = async function (request, response) {
 const getSingleBook = async function (request, response) {
     try {
         const slug = request.params.slug;
-        console.log(slug);
         if (!slug) {
             return response.status(500).json({status: 'error', data: 'Book slug is empty.'});
         }
@@ -36,7 +35,43 @@ const getSingleBook = async function (request, response) {
     }
 };
 
+const createNewBook = async function (request, response) {
+    try {
+        const bookDetails = request.body;
+        if (!bookDetails.title) {
+            return response.status(500).json({status: 'error', data: 'Book title is empty.'});
+        } else if (!bookDetails.slug) {
+            return response.status(500).json({status: 'error', data: 'Book slug is empty.'});
+        } else if (!bookDetails.stars) {
+            return response.status(500).json({status: 'error', data: 'Book star is empty.'});
+        } else if (!bookDetails.description) {
+            return response.status(500).json({status: 'error', data: 'Book description is empty.'});
+        } else if (!bookDetails.category) {
+            return response.status(500).json({status: 'error', data: 'Book category is empty.'});
+        } else if (!bookDetails.thumbnail) {
+            return response.status(500).json({status: 'error', data: 'Book thumbnail is empty.'});
+        }
+        const newBook = new BookModel({
+            title: bookDetails.title,
+            slug: bookDetails.slug,
+            stars: bookDetails.stars,
+            description: bookDetails.description,
+            category: bookDetails.category,
+            thumbnail: bookDetails.thumbnail
+        });
+        const data = await BookModel.create(newBook);
+        if (!data) {
+            return response.status(500).json({status: 'error', data: 'Book creation failed.'});
+        }
+        return response.status(200).json({status: 'success', data: data});
+    } catch (error) {
+        console.log(error);
+        return response.status(500).json({status: 'error', data: 'An error occured while fetching books details'});
+    }
+};
+
 module.exports = {
     getAllBooks,
-    getSingleBook
+    getSingleBook,
+    createNewBook
 };
--- src/routes/v1/Books.js
+++ src/routes/v1/Books.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const BooksController = require('../../controllers/Books');
 
 router.route('/books').get(BooksController.getAllBooks);
+router.route('/books/add').post(BooksController.createNewBook);
 router.route('/books/:slug').get(BooksController.getSingleBook);
 
 module.exports = router;
